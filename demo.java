3a392f7fb549efc4ef3d20f1845cb08ac611f410
Durgesh88mlg <durgesh88mlg@gmail.com>
2025-02-22 20:57:41 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,7 +2,8 @@ public class demo {
     public static void main(String[] args) {
         // Print a message to the console as output
         System.out.println("Hello welcome to jenkins");
-        System.out.println("byee");    
+          
+        
     } 
 }  
 
